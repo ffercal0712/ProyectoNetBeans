ef11b4faff44b573059fc6c19eeecec10b96b731
ffercal0712 <ffercal0712@g.educaand.es>
2025-04-22 11:59:14 +0200

    Primer commit en NetBeans

---
+++ ProyectoNetBeansGit/src/proyectonetbeansgit/ProyectoNetBeansGit.java
@@ -0,0 +1,11 @@
+package proyectonetbeansgit;
+
+public class ProyectoNetBeansGit {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Primer commit");
+        
+    }
+    
+}
