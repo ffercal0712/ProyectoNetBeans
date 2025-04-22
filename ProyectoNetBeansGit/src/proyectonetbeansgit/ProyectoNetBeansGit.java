112538d101b9ce6f624438d2b5b3884dcb4287b9
ffercal0712 <ffercal0712@g.educaand.es>
2025-04-22 12:04:36 +0200

    Segundo commit en NetBeans

--- ProyectoNetBeansGit/src/proyectonetbeansgit/ProyectoNetBeansGit.java
+++ ProyectoNetBeansGit/src/proyectonetbeansgit/ProyectoNetBeansGit.java
@@ -5,6 +5,7 @@ public class ProyectoNetBeansGit {
     public static void main(String[] args) {
         
         System.out.println("Primer commit");
+        System.out.println("Segundo commit");
         
     }
     
